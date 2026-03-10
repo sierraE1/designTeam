b374c16ca9c3778ea61fcb8ef554644c19b7402f
florida <floridaflk@gmail.com>
2026-03-10 18:37:10 -0400

    changed mood_service

--- backend/app/services/mood_service.py
+++ backend/app/services/mood_service.py
@@ -1,5 +1,20 @@
 from app.schemas import MoodCreate
+from app.models import Mood
+
+moods = []
+next_id = 1
 
 
-def save_mood(payload: MoodCreate) -> dict[str, str]:
-    return {"message": f"Mood '{payload.value}' saved"}
+def save_mood(payload: MoodCreate) -> Mood:
+    global next_id
+
+    mood = Mood(
+        id=next_id,
+        user_id=payload.user_id,
+        value=payload.value
+    )
+
+    moods.append(mood)
+    next_id += 1
+
+    return mood
