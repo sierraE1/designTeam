1d2f3a5e8c34ff9affe21432f76c40f160d1848d
florida <floridaflk@gmail.com>
2026-03-10 18:37:50 -0400

    changed mood.py

--- backend/app/routes/mood.py
+++ backend/app/routes/mood.py
@@ -1,11 +1,10 @@
 from fastapi import APIRouter
-
-from app.schemas import MoodCreate
+from app.schemas import MoodCreate, MoodResponse
 from app.services.mood_service import save_mood
 
 router = APIRouter()
 
 
-@router.post("/")
+@router.post("/", response_model=MoodResponse)
 def save_mood_endpoint(payload: MoodCreate):
     return save_mood(payload)
