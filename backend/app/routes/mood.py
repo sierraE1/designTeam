fb4ff3ea536db798707759d3373ff73ca348c4b5
florida <floridaflk@gmail.com>
2026-03-22 18:20:58 -0400

    fixed create mood endpoint

--- backend/app/routes/mood.py
+++ backend/app/routes/mood.py
@@ -1,9 +1,25 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, status
 from app.schemas import MoodCreate, MoodResponse
-from app.services.mood_service import create_mood
 
-router = APIRouter()
+router = APIRouter(
+    # prefix="/mood",
+    tags=["mood"]
+)
 
-@router.post("/", response_model=MoodResponse)
-def save_mood_endpoint(payload: MoodCreate):
-    return create_mood(payload)
+# Temporary in-memory storage
+moods_db = []
+
+@router.post("/", response_model=MoodResponse, status_code=status.HTTP_201_CREATED)
+def create_mood_endpoint(payload: MoodCreate):
+    new_id = len(moods_db) + 1
+    mood = {
+        "id": new_id,
+        "user_id": payload.user_id,
+        "value": payload.value
+    }
+    moods_db.append(mood)
+    return mood
+
+@router.get("/", response_model=list[MoodResponse])
+def get_moods():
+    return moods_db
